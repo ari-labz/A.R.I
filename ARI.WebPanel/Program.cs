0650b0eb3e44319b0671807ea14dcc4fc2411769
Xywren <xywren@xywren.net>
2026-05-26 22:57:30 +0100

    Register VoiceSynthesisService as singleton
    Add dependency injection for application-lifetime voice synthesis service

--- ARI.WebPanel/Program.cs
+++ ARI.WebPanel/Program.cs
@@ -1,7 +1,15 @@
+using ARI.VoiceSynthesis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<VoiceSynthesisService>(provider =>
+{
+    string rvcPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "external", "rvc");
+    return new VoiceSynthesisService(rvcPath);
+});
 
 var app = builder.Build();
 
@@ -9,7 +17,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -21,9 +28,8 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
