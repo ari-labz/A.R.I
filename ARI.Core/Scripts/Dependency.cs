13dc31ea8dbb6ce40c183bf93942882e474028b3
Xywren <xywren@xywren.net>
2026-05-27 21:54:20 +0100

    Handle missing dependencies in startup
    Add error handling for docker and python executables

--- ARI.Core/Scripts/Dependency.cs
+++ ARI.Core/Scripts/Dependency.cs
@@ -7,23 +7,38 @@ public class Dependency
     
     public static async Task CheckDocker()
     {
-        Process process = Common.RunCommand("docker", "--version");
-        await process.WaitForExitAsync();
+        try
+        {
+            Process process = Common.RunCommand("docker", "--version");
+            await process.WaitForExitAsync();
 
-        if (process.ExitCode != 0)
+            if (process.ExitCode != 0)
+
+            Console.WriteLine("Docker is installed.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Docker is not installed. Please install Docker Desktop from https://docker.com and try again.");   
             throw new Exception("Docker is not installed. Please install Docker Desktop from https://docker.com and try again.");
-
-        Console.WriteLine("Docker is installed.");
+        }
     }
 
     public static async Task CheckPython()
     {
-        Process process = Common.RunCommand("python3", "--version");
-        await process.WaitForExitAsync();
+        try
+        {
+            Process process = Common.RunCommand("python3", "--version");
+            await process.WaitForExitAsync();
 
-        if (process.ExitCode != 0)
+            if (process.ExitCode != 0)
+                throw new Exception("");
+
+            Console.WriteLine("Python is installed.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Python is not installed. Please install Python and try again.");
             throw new Exception("Python is not installed. Please install Python and try again.");
-
-        Console.WriteLine("Python is installed.");
+        }
     }
 }
