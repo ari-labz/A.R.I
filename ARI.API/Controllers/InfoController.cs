fa9a641c452e9f5845e4e772689ebb1f2804b653
Xywren <xywren@xywren.net>
2026-07-09 10:25:04 +0100

    v0.5.0

--- ARI.API/Controllers/InfoController.cs
+++ ARI.API/Controllers/InfoController.cs
@@ -7,7 +7,7 @@ namespace ARI.API.Controllers;
 public class InfoController : ControllerBase
 {
     // Bump this whenever a breaking change requires clients to update.
-    private const string RequiredClientVersion = "0.4.3";
+    private const string RequiredClientVersion = "0.5.0";
 
     [HttpGet("version")]
     public IActionResult GetVersion() =>
